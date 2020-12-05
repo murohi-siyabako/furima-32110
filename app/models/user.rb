86b118cd1fd542dede2b2825f9ca805ddd9eedd8
murohi-siyabako <ryun19901208hk@yahoo.co.jp>
2020-12-05 15:32:06 +0900

    ユーザー管理機能 修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email
-    validates :password
     validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Include both letters and numbers."}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,15 +9,6 @@ RSpec.describe User, type: :model do
       it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
         expect(@user).to be_valid
       end
-      it "nicknameが6文字以下で登録できる" do
-        @user.nickname = "aaaaaa"
-        expect(@user).to be_valid
-      end
-      it "passwordが6文字以上であれば登録できる" do
-        @user.password = "000000"
-        @user.password_confirmation = "000000"
-        expect(@user).to be_valid
-      end
     end
     context '新規登録がうまくいかない時' do
       it "nicknameが空だと登録できない" do
